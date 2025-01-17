45305c001c2e1347d510fae1ace9ef1f7a2d94d0
dasturchi8520 <dasturchi174@gmail.com>
2025-01-17 10:57:20 +0500

    Add files via upload

---
+++ site/Dproject 1/script.js
@@ -0,0 +1,12 @@
+const content = document.querySelector(".content")
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click" , ()  =>{
+    content.style =
+    "background-color:black"
+
+});
+btn.addEventListener("click" , ()  =>{
+   btn.style =
+   "color:black"
+});
